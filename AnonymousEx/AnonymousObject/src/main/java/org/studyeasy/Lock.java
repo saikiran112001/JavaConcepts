cc9b7f3f6f5169328cc96cac6a3572f7107ab349
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-09-25 11:22:44 +0530

    Anonymous

---
+++ AnonymousEx/AnonymousObject/src/main/java/org/studyeasy/Lock.java
@@ -0,0 +1,10 @@
+package org.studyeasy;
+
+public class Lock {
+    String code="123456";
+
+    public String getCode() {
+        return code;
+    }
+}
+
---
+++ AnonymousEx/AnonymousObject/src/main/java/org/studyeasy/Main.java
@@ -0,0 +1,13 @@
+package org.studyeasy;
+
+public class Main {
+    public static void main (String[] args){
+
+        if(new Lock().getCode()== "123456"){
+            System.out.println("the door is open now");
+        }
+        else{
+            System.out.println("the door is close");
+        }
+    }
+}
