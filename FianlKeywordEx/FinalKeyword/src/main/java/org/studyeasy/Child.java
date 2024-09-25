dcaff4c2a22300948cf807676ce19d6fcb5b408f
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-09-25 18:45:14 +0530

    Final keyword

---
+++ FianlKeywordEx/FinalKeyword/src/main/java/org/studyeasy/Child.java
@@ -0,0 +1,19 @@
+package org.studyeasy;
+
+public class Child {
+    public final int x;
+
+    public Child() {
+        this.x=10;
+    }
+
+
+    public Child(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+}
---
+++ FianlKeywordEx/FinalKeyword/src/main/java/org/studyeasy/Main.java
@@ -0,0 +1,11 @@
+package org.studyeasy;
+
+public class Main {
+    public static void main(String[] args){
+
+        Child child=new Child();
+        System.out.println(child.x);
+
+
+    }
+}
