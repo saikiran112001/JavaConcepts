517699c7baef732d8de76442456e089190f6ad41
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-10-04 10:35:38 +0530

    AutoboxingUnboxing

--- BocingEx1/AutoboxingAndUnboxing/src/main/java/org/studyeasy/Main.java
+++ BocingEx1/AutoboxingAndUnboxing/src/main/java/org/studyeasy/Main.java
@@ -20,11 +20,11 @@ class IntWrapper{
 public class Main {
     public static void main(String[] args){
         ArrayList<Integer> numbersList =new ArrayList<>();
-        numbersList.add(27);
+        numbersList.add(25);
         System.out.println(numbersList.get(0));
 
         ArrayList<IntWrapper> numbersList2 =new ArrayList<>();
-        numbersList2.add(new IntWrapper(67));
+        numbersList2.add(new IntWrapper(65));
         System.out.println(numbersList2.get(0).getNum());
 
 
